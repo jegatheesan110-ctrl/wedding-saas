7fe40d462606e4e56ac5aa3d2de08a40ef555f77
jegatheesan110-ctrl <jegatheesan110@gmail.com>
2026-06-06 13:51:45 +0530

    add google search console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     ]
   },
   verification: {
-    google: "YOUR_GOOGLE_SEARCH_CONSOLE_VERIFICATION_TAG",
+    google: "xtUh6TyPUhLrT9oaLk1TqZYjY1JR11MtnHJNxMAGWSk",
   }
 };
 
